df110f9ff5cf7caf7052f85d616c57682aa17974
Guilherme Bonald <guilhermebonald@hotmail.com>
2024-03-10 10:02:58 -0300

    Update

--- src/route.py
+++ src/route.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 route_bp = Blueprint("route", __name__)
 
 
+# Access with token validate.
 @route_bp.route("/secret", methods=["GET"])
 def secret_route():
 
@@ -13,11 +14,18 @@ def secret_route():
     if not raw_token:
         return jsonify({"error": "Não Autorizado"}), 401
 
+    try:
+        token = raw_token.split()[1]
+        token_information = jwt.decode(token, key="1234", algorithms="HS256")
+    except jwt.InvalidSignatureError:
+        return jsonify({"error": "Token Invalido"}), 401
+
     return jsonify({"data": "Mensagem secreta"}), 200
 
 
+# Generate token with auth.
 @route_bp.route("/auth", methods=["POST"])
-def authorization_route():
+def auth_gentoken_route():
     token = jwt.encode(
         {"exp": datetime.utcnow() + timedelta(minutes=30)},
         key="1234",
